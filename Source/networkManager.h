bb5ed1a5d01bf3fcbd4653e0e188234b1d308b16
DSync <haroun7@gmail.com>
2015-03-23 11:33:30 +0530

    threw in the tuple include.

--- Source/networkManager.h
+++ Source/networkManager.h
@@ -1,11 +1,15 @@
 #ifndef NETWORK_MANAGER
 #define NETWORK_MANAGER
 
+
 /* TODO: Include boost sockets, threads, mention library includes and so on.
 	Look at helloUDP in the Hello directory ~
 */
 
 /** The header file for network manager. */
+
+#include <tuple>
+using namespace std;
 typedef tIP tuple< int,int,int,int >
 
 class NetworkManager {
