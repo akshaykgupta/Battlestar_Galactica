2e4954e93bd4c9545f4a6531b034f0bb347169db
DSync <haroun7@gmail.com>
2015-04-02 12:53:20 +0530

    preparing for assimp

--- Source/spaceObject.hpp
+++ Source/spaceObject.hpp
@@ -2,11 +2,45 @@
 #define SPACE_OBJECT_HPP
 /* bullets! */
 #include <btBulletDynamicsCommon.h>
+#include "helpers.hpp"
 
 class SpaceObject {
 public:
+	/* constructors */
+	SpaceObject();
+	~SpaceObject();
+	/** functions for loading, etc. */
+	void init();
+		void physics_init();
+		void render_init();
+
+	void loadRenderGeometry(std::string _path);
+		//void render_geometry(); declared in the rendering section.
+
+	/* for debugging, if done. */
+	void print(bool debug_flag = false);
+	
+	/* for rendering into opengl around the centre itself. */
+	void render(bool debuf_flag = false);
+		void render_geometry();
+			void finalizeVBO();
+			void bindVBO();
+		void mesh_render(bool dflag = false);
+
+
 private:
-	btBroadphaseInterface* parent_broadphase;
+	/* misc data */
+	OBJECT_TYPE obj_type;
+	OBJPath objpath;
+	PHYPath phypath;
+
+	/*user pointer types*/
+
+	/*physics*/
+	btCompoundShape* mesh;
+	btRigidBody* body;
+	/*render*/
+
 };
 
 #endif
