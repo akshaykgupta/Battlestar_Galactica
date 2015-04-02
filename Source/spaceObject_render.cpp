dc2a758180b1eb659c864d7ba7d92139854c7fcf
DSync <haroun7@gmail.com>
2015-04-02 17:04:35 +0530

    jugadu render

--- Source/spaceObject_render.cpp
+++ Source/spaceObject_render.cpp
@@ -11,9 +11,74 @@ void SpaceObject::render_physics(bool dflag) {
 	body->getMotionState()->getWorldTransform(trans_com);
 	float mat_gl[16];
 	trans_com.getOpenGLMatrix(mat_gl);
-	glPushMatrix(mat_gl);
+	glPushMatrix();
 		glMultMatrix(mat_gl);
 		//TODO for each cuboid, render the shape.
+		for(int i=0; i<children.size(); ++i) {
+			float mat_child[16];
+			childrenTransform[i].getOpenGLMatrix(mat_child);
+			glPushMatrix();
+			glMultMatrix(mat_child);
+				btvector3 = children->getHalfExtentsWithMargin();
+				drawBox(btVector3& hdim);
+			glPopMatrix();
+		}
 	glPopMatrix();
+}
+
+//not properly rendered.
+void SpaceObject::drawBox(btVector3& dim) {
+	btScalar x,y,z;
+	x = dim.getX();
+	y = dim.getY();
+	z = dim.getZ();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(-x,y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(x,-y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(x,y,-z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,-z);
+		glVertex3f(-x,-y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,-z);
+		glVertex3f(-x,y,-z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,-z);
+		glVertex3f(x,-y,-z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,z);
+		glVertex3f(-x,y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,z);
+		glVertex3f(x,-y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(-x,-y,z);
+		glVertex3f(x,y,-z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(x,y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(x,y,z);
+	glEnd();
+	glBegin(GL_LINE);
+		glVertex3f(x,y,z);
+		glVertex3f(x,y,z);
+	glEnd();
 }
 #endif
