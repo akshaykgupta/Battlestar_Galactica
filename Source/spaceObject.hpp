f903bd545495460de81b4d9b16b9c802d31d5b30
DSync <haroun7@gmail.com>
2015-04-02 11:55:36 +0530

    we dont need cuboid no more.

---
+++ Source/spaceObject.hpp
@@ -0,0 +1,12 @@
+#ifndef SPACE_OBJECT_HPP
+#define SPACE_OBJECT_HPP
+/* bullets! */
+#include <btBulletDynamicsCommon.h>
+
+class SpaceObject {
+public:
+private:
+	btBroadphaseInterface* parent_broadphase;
+};
+
+#endif
