ac6bda83d39efcb9cf3f2399d66b759880451109
DSync <haroun7@gmail.com>
2015-04-06 18:29:52 +0530

    switching work to brun's mac

---
+++ Source/skybox.hpp
@@ -0,0 +1,13 @@
+#ifndef SKYBOX_CPP
+#define SKYBOX_CPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+class SkyBox {
+private:
+public:
+};
+#endif
