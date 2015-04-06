62752ca567e7cfd0d9338a80ce8231b766aeae57
codedecde <barunpatra95@gmail.com>
2015-04-06 19:24:03 +0530

    Skybox

--- Source/skybox.hpp
+++ Source/skybox.hpp
@@ -8,6 +8,21 @@
 
 class SkyBox {
 private:
+	sf::Image* box; 
+	sf::Texture* texture;
+	string imgPath;
+	enum FACE {
+		TOP = 0,
+		BOTTOM = 1,
+		LEFT = 2,
+		RIGHT = 3,
+		FRONT = 4,
+		BACK = 5
+	};
 public:
+	SkyBox(const string &);
+	~SkyBox();
+	void setImage();
+	void renderBox(float size);
 };
 #endif
